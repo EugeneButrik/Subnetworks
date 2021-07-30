2ec5ed4da64629956f71916785e9d3fb985be06f
Eugene Butrik <butrike@gmail.com>
2021-07-31 00:31:40 +0300

    Minor style changes

--- scripts/animation.js
+++ scripts/animation.js
@@ -2,8 +2,8 @@
 
 function draw() {
     let canvas = document.getElementById("canvas");
-   
-    if (!canvas.getContext) { return }; // Canvas support check
+
+    if (!canvas.getContext) { return }; // Canvas support check (obsolete)
 
     let ctx = canvas.getContext("2d");
     let draging = false;
@@ -57,8 +57,9 @@ function draw() {
     });
 
     canvas.addEventListener('wheel', function (e) {
-        rectangle.width += e.deltaY * 0.1;
-        rectangle.height += e.deltaY * 0.1;
+        rectangle.width += +(e.deltaY * 0.1).toFixed();
+        rectangle.height += +(e.deltaY * 0.1).toFixed();
+        // Both prior values need to be rounded due to precision loss (found in Firefox)
 
         render();
     });
@@ -76,16 +77,38 @@ function draw() {
         ctx.font = `${fontSize}px courier`;
 
         ctx.fillText(`${draging ? "Draging" : "Not draging"}`, 10, fontSize);
-        ctx.fillText(`Mouse position:     [${mousePos.x}, ${mousePos.y}]`, 10, fontSize * 2);
-        ctx.fillText(`Rectangle position: [${rectangle.x}, ${rectangle.y}]`, 10, fontSize * 3);
-        ctx.fillText(`Mouse offset:       [${mouseOffset.x}, ${mouseOffset.y}]`, 10, fontSize * 4);
-        ctx.fillText(`Rectangle size:     [${rectangle.width} x ${rectangle.height}]`, 10, fontSize * 5);
-        ctx.fillText(`Window size:        [${document.documentElement.clientWidth} x ${document.documentElement.clientHeight}]`, 10, fontSize * 6);
+        ctx.fillText(
+            `Mouse position:    ` +
+            `[${mousePos.x}, ${mousePos.y}]`,
+            10, fontSize * 2
+        );
+        ctx.fillText(
+            `Rectangle position:` +
+            `[${rectangle.x}, ${rectangle.y}]`,
+            10, fontSize * 3
+        );
+        ctx.fillText(
+            `Mouse offset:      ` +
+            `[${mouseOffset.x}, ${mouseOffset.y}]`,
+            10, fontSize * 4
+        );
+        ctx.fillText(
+            `Rectangle size:    ` +
+            `[${rectangle.width} x ${rectangle.height}]`,
+            10, fontSize * 5
+        );
+        ctx.fillText(
+            `Window size:       ` +
+            `[${document.documentElement.clientWidth}` +
+            ` x ` +
+            `${document.documentElement.clientHeight}]`,
+            10, fontSize * 6
+        );
     };
 
     function prepareCanvas() {
-        canvas.width = document.documentElement.clientWidth - 16;
-        canvas.height = document.documentElement.clientHeight - 20;
+        canvas.width = document.documentElement.clientWidth;
+        canvas.height = document.documentElement.clientHeight - 4;
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     };
