bc4ae429855ee8625caee1394e64ee4f4b63ca31
Eugene Butrik <butrike@gmail.com>
2021-08-11 14:29:01 +0300

    Code refactoring. Removed all semicolons.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
-`use strict`;
+`use strict`
 
-let DEBUG = 1;
+let DEBUG = 1
 
 let canvas = {
 	element: document.getElementById("canvas"),
 	context: document.getElementById("canvas").getContext("2d"),
 
 	resize: function () {
-		this.element.width = window.innerWidth;
-		this.element.height = window.innerHeight;
+		this.element.width = window.innerWidth
+		this.element.height = window.innerHeight
 	},
 
 	clear: function () {
@@ -17,9 +17,9 @@ let canvas = {
 			0,
 			this.element.width,
 			this.element.height
-		);
+		)
 	},
-};
+}
 
 let state = {
 	panning: false,
@@ -33,7 +33,7 @@ let state = {
 		h: 0,
 		v: 0,
 	},
-};
+}
 
 let infoPanel = {
 	logStrings: [],
@@ -43,23 +43,23 @@ let infoPanel = {
 
 	log: function (s) {
 		if (s == this.logLastMessage) {
-			this.logStrings[0] = `|${++this.repeated}| ` + s;
+			this.logStrings[0] = `|${++this.repeated}| ` + s
 		} else {
-			this.logStrings.unshift(s);
+			this.logStrings.unshift(s)
 
-			this.logLastMessage = s;
-			this.repeated = 1;
-		};
+			this.logLastMessage = s
+			this.repeated = 1
+		}
 
 		if (this.logStrings.length > this.logMaxStrings) {
-			this.logStrings.pop();
-		};
+			this.logStrings.pop()
+		}
 	},
 
 	draw: function () {
-		canvas.context.fillStyle = "rgba(0, 0, 0, 1)";
-		let fontSize = canvas.element.height / 60;
-		canvas.context.font = `${fontSize}px courier`;
+		canvas.context.fillStyle = "rgba(0, 0, 0, 1)"
+		let fontSize = canvas.element.height / 60
+		canvas.context.font = `${fontSize}px courier`
 
 		let panelStrings = [
 			`${state.panning ? "Panning" : "Not panning"}`,
@@ -71,347 +71,347 @@ let infoPanel = {
 			`Pan displacement:          ` +
 			`[${state.panDisplacement.h}, ` +
 			`${state.panDisplacement.v}]`,
-		];
+		]
 
 		for (let s in panelStrings) {
 			canvas.context.fillText(
 				panelStrings[s],
 				10,
 				10 + (fontSize * (+(s) + 1))
-			);
-		};
+			)
+		}
 
 		for (let s in this.logStrings) {
 			canvas.context.fillText(
 				this.logStrings[s],
 				10,
 				canvas.element.height - (fontSize * (+(s) + 1))
-			);
-		};
+			)
+		}
 	},
-};
+}
 
 class subnet {
 	position = {
 		h: undefined,
 		v: undefined,
-	};
+	}
 
-	width;
-	height;
-	color = "rgba(200, 0, 0, 0.5)";
-	positionInColumn;
-	IP;
-	mask;
-	label;
+	width
+	height
+	color = "rgba(200, 0, 0, 0.5)"
+	positionInColumn
+	IP
+	mask
+	label
 
-	constructor() { };
+	constructor() { }
 
 	draw() {
-		canvas.context.fillStyle = this.color;
+		canvas.context.fillStyle = this.color
 
 		canvas.context.strokeRect(
 			this.position.h,
 			this.position.v,
 			this.width,
 			this.height
-		);
+		)
 
 		canvas.context.fillRect(
 			this.position.h,
 			this.position.v,
 			this.width,
 			this.height
-		);
+		)
 
-		this.getLabelFromIPAndMask();
+		this.getLabelFromIPAndMask()
 
-		canvas.context.fillStyle = "rgba(0, 0, 0, 1)";
+		canvas.context.fillStyle = "rgba(0, 0, 0, 1)"
 
-		let fontSize = canvas.element.height / 60;
+		let fontSize = canvas.element.height / 60
 
-		canvas.context.font = `${fontSize}px courier`;
+		canvas.context.font = `${fontSize}px courier`
 
 		canvas.context.fillText(
 			this.label,
 			this.position.h + fontSize * 0.75,
 			this.position.v + fontSize * 1.5,
-		);
-	};
+		)
+	}
 
 	getLabelFromIPAndMask() {
 		let octet1 =
-			Math.floor(this.IP / 256 ** 3);
+			Math.floor(this.IP / 256 ** 3)
 		let octet2 =
-			Math.floor((this.IP % 256 ** 3) / 256 ** 2);
+			Math.floor((this.IP % 256 ** 3) / 256 ** 2)
 		let octet3 =
-			Math.floor((this.IP % 256 ** 3) % 256 ** 2 / 256);
+			Math.floor((this.IP % 256 ** 3) % 256 ** 2 / 256)
 		let octet4 =
-			Math.floor((this.IP % 256 ** 3) % 256 ** 2 % 256);
+			Math.floor((this.IP % 256 ** 3) % 256 ** 2 % 256)
 		this.label =
 			`${octet1}.${octet2}.${octet3}.${octet4} / ${this.mask}`
-	};
-};
+	}
+}
 
 class column {
 	position = {
 		h: undefined,
 		v: undefined,
-	};
+	}
 
-	subnets = [];
-	elementWidth;
-	elementHeight;
+	subnets = []
+	elementWidth
+	elementHeight
 
 	/**
 	 * The distance from the pointer to the column origin vertical
 	 * position, measured in pixels
 	*/
-	onScreenOffset = 0;
+	onScreenOffset = 0
 
 	/**
 	 * The distance from the pointer to the column origin vertical
 	 * position, measured in its elements heights
 	*/
-	relativeOffset = 0;
+	relativeOffset = 0
 
-	constructor() { };
+	constructor() { }
 
 	draw() {
-		this.position.h += state.panDisplacement.h;
+		this.position.h += state.panDisplacement.h
 
 		if (state.panning) {
 			if (this.relativeOffset == 0) {
 				this.relativeOffset =
 					(this.position.v - state.pointerPosition.v) /
-					this.getElementHeight(this.position.h);
-			};
+					this.getElementHeight(this.position.h)
+			}
 		} else {
-			this.onScreenOffset = 0;
-			this.relativeOffset = 0;
-		};
+			this.onScreenOffset = 0
+			this.relativeOffset = 0
+		}
 
 		let newOnScreenOffset =
 			this.relativeOffset *
-			this.getElementHeight(this.position.h);
+			this.getElementHeight(this.position.h)
 
-		let onScreenOffsetIncrement;
+		let onScreenOffsetIncrement
 
 		if (this.onScreenOffset != 0) {
 			onScreenOffsetIncrement =
-				newOnScreenOffset - this.onScreenOffset;
+				newOnScreenOffset - this.onScreenOffset
 		} else {
-			onScreenOffsetIncrement = 0;
-		};
+			onScreenOffsetIncrement = 0
+		}
 
-		this.onScreenOffset = newOnScreenOffset;
+		this.onScreenOffset = newOnScreenOffset
 
 		this.position.v +=
-			state.panDisplacement.v + onScreenOffsetIncrement;
+			state.panDisplacement.v + onScreenOffsetIncrement
 
 		this.elementWidth =
-			canvas.element.width / columnsOnScreen;
+			canvas.element.width / columnsOnScreen
 		this.elementHeight =
-			this.getElementHeight(this.position.h);
+			this.getElementHeight(this.position.h)
 
 		for (let s in this.subnets) {
-			this.subnets[s].width = this.elementWidth;
-			this.subnets[s].height = this.elementHeight;
+			this.subnets[s].width = this.elementWidth
+			this.subnets[s].height = this.elementHeight
 
 			this.subnets[s].position.h =
-				this.position.h - this.elementWidth / 2;
+				this.position.h - this.elementWidth / 2
 			this.subnets[s].position.v =
 				this.position.v + this.subnets[s].height *
-				this.subnets[s].positionInColumn;
+				this.subnets[s].positionInColumn
 
-			this.subnets[s].draw();
-		};
-	};
+			this.subnets[s].draw()
+		}
+	}
 
 	getElementHeight(horPosition) {
 		let baseElementHeight =
-			canvas.element.height / rowsInMainColumn;
+			canvas.element.height / rowsInMainColumn
 		let elementHeight =
-			baseElementHeight * this.getVertScale(horPosition);
+			baseElementHeight * this.getVertScale(horPosition)
 
-		return elementHeight;
-	};
+		return elementHeight
+	}
 
 	getVertScale(horPosition) {
 		let horPositionRelativeToCanvasMiddle =
-			horPosition - canvas.element.width / 2;
-		let columnWidth = canvas.element.width / columnsOnScreen;
-		let power = -horPositionRelativeToCanvasMiddle / columnWidth;
+			horPosition - canvas.element.width / 2
+		let columnWidth = canvas.element.width / columnsOnScreen
+		let power = -horPositionRelativeToCanvasMiddle / columnWidth
 		/*
 		The relative horizontal position must be negative for the
 		inverse direction of vertical scale change (small subnets are
 		placed to the left of large)
 		*/
-		let scale = 1 / 2 ** power;
+		let scale = 1 / 2 ** power
 
-		return scale;
-	};
+		return scale
+	}
 
 	appendBottom() {
-		let newSubnet = new subnet();
+		let newSubnet = new subnet()
 
 		if (this.subnets.length != 0) {
-			let last = this.subnets.length - 1;
+			let last = this.subnets.length - 1
 
 			newSubnet.positionInColumn =
-				+(this.subnets[last].positionInColumn) + 1;
+				+(this.subnets[last].positionInColumn) + 1
 
 			newSubnet.IP =
 				+(this.subnets[last].IP) +
-				(2 ** 32 / 2 ** this.subnets[last].mask);
+				(2 ** 32 / 2 ** this.subnets[last].mask)
 
-			newSubnet.mask = this.subnets[last].mask;
+			newSubnet.mask = this.subnets[last].mask
 		} else {
-			newSubnet.positionInColumn = 0;
+			newSubnet.positionInColumn = 0
 
-			newSubnet.IP = 3232235520;
-			newSubnet.mask = 24;
-		};
+			newSubnet.IP = 3232235520
+			newSubnet.mask = 24
+		}
 
-		this.subnets.push(newSubnet);
-	};
+		this.subnets.push(newSubnet)
+	}
 
 	appendTop() {
-		let newSubnet = new subnet();
+		let newSubnet = new subnet()
 
 		if (this.subnets.length != 0) {
 			newSubnet.positionInColumn =
-				+(this.subnets[0].positionInColumn) - 1;
+				+(this.subnets[0].positionInColumn) - 1
 
 			newSubnet.IP =
 				+(this.subnets[0].IP) -
-				(2 ** 32 / 2 ** this.subnets[0].mask);
+				(2 ** 32 / 2 ** this.subnets[0].mask)
 
-			newSubnet.mask = this.subnets[0].mask;
+			newSubnet.mask = this.subnets[0].mask
 		} else {
-			newSubnet.positionInColumn = 0;
-			newSubnet.IP = 3232235520;
-			newSubnet.mask = 24;
-		};
+			newSubnet.positionInColumn = 0
+			newSubnet.IP = 3232235520
+			newSubnet.mask = 24
+		}
 
-		this.subnets.unshift(newSubnet);
-	};
+		this.subnets.unshift(newSubnet)
+	}
 
 	cutBottom() {
-		this.subnets.pop();
-	};
+		this.subnets.pop()
+	}
 
 	cutTop() {
-		this.subnets.shift();
-	};
-};
+		this.subnets.shift()
+	}
+}
 
 
 
-canvas.resize();
+canvas.resize()
 
-let columnsOnScreen = 3;
-let rowsInMainColumn = 10;
+let columnsOnScreen = 3
+let rowsInMainColumn = 10
 
-let testColumn = new column();
-testColumn.position.h = int(canvas.element.width / 2);
-testColumn.position.v = int(canvas.element.height / 2);
+let testColumn = new column()
+testColumn.position.h = int(canvas.element.width / 2)
+testColumn.position.v = int(canvas.element.height / 2)
 
-testColumn.appendBottom();
-testColumn.appendBottom();
-testColumn.appendBottom();
-testColumn.appendTop();
-testColumn.appendTop();
+testColumn.appendBottom()
+testColumn.appendBottom()
+testColumn.appendBottom()
+testColumn.appendTop()
+testColumn.appendTop()
 
-render();
+render()
 
 
 
 window.addEventListener('resize', function (e) {
-	canvas.resize();
+	canvas.resize()
 
-	render();
-}, false);
+	render()
+}, false)
 
 window.addEventListener('contextmenu', function (e) {
-	e.preventDefault();
+	e.preventDefault()
 
-	render();
-}, false);
+	render()
+}, false)
 
 window.addEventListener('keydown', function (e) {
 	if (e.code == `ArrowUp`) {
-		state.panning = true;
-		state.panDisplacement.v = -10;
-	};
+		state.panning = true
+		state.panDisplacement.v = -10
+	}
 
 	if (e.code == `ArrowDown`) {
-		state.panning = true;
-		state.panDisplacement.v = 10;
-	};
+		state.panning = true
+		state.panDisplacement.v = 10
+	}
 
 	if (e.code == `ArrowRight`) {
-		state.panning = true;
-		state.panDisplacement.h = 10;
-	};
+		state.panning = true
+		state.panDisplacement.h = 10
+	}
 
 	if (e.code == `ArrowLeft`) {
-		state.panning = true;
-		state.panDisplacement.h = -10;
-	};
+		state.panning = true
+		state.panDisplacement.h = -10
+	}
 
-	render();
-}, false);
+	render()
+}, false)
 
 window.addEventListener('keyup', function (e) {
-	state.panning = false;
-	state.panDisplacement.h = 0;
-	state.panDisplacement.v = 0;
+	state.panning = false
+	state.panDisplacement.h = 0
+	state.panDisplacement.v = 0
 
-	render();
-}, false);
+	render()
+}, false)
 
 window.addEventListener('mousedown', function (e) {
-	e.preventDefault();
+	e.preventDefault()
 
-	state.panning = true;
+	state.panning = true
 
-	render();
-}, false);
+	render()
+}, false)
 
 window.addEventListener('mouseup', function (e) {
-	e.preventDefault();
+	e.preventDefault()
 
-	state.panning = false;
+	state.panning = false
 
-	state.panDisplacement.h = 0;
-	state.panDisplacement.v = 0;
+	state.panDisplacement.h = 0
+	state.panDisplacement.v = 0
 
-	render();
-}, false);
+	render()
+}, false)
 
 window.addEventListener('mousemove', function (e) {
-	e.preventDefault();
+	e.preventDefault()
 
 	if (e.buttons == 0) {
-		state.panning = false;
+		state.panning = false
 	} else {
-		state.panning = true;
-	};
+		state.panning = true
+	}
 
 	if (state.panning) {
-		state.panDisplacement.h = e.clientX - state.pointerPosition.h;
-		state.panDisplacement.v = e.clientY - state.pointerPosition.v;
+		state.panDisplacement.h = e.clientX - state.pointerPosition.h
+		state.panDisplacement.v = e.clientY - state.pointerPosition.v
 	} else {
-		state.panDisplacement.h = 0;
-		state.panDisplacement.v = 0;
-	};
+		state.panDisplacement.h = 0
+		state.panDisplacement.v = 0
+	}
 
-	state.pointerPosition.h = e.clientX;
-	state.pointerPosition.v = e.clientY;
+	state.pointerPosition.h = e.clientX
+	state.pointerPosition.v = e.clientY
 
-	render();
-}, false);
+	render()
+}, false)
 
 canvas.element.addEventListener('wheel', function (e) {
 	/*
@@ -422,70 +422,70 @@ canvas.element.addEventListener('wheel', function (e) {
 	mouse wheel events, e.g. zooming with Ctrl key pressed.
 	*/
 
-	e.preventDefault();
+	e.preventDefault()
 
 	if (e.deltaY < 0 && !e.altKey) {
-		testColumn.cutBottom();
-	};
+		testColumn.cutBottom()
+	}
 
 	if (e.deltaY > 0 && !e.altKey) {
-		testColumn.appendBottom();
-	};
+		testColumn.appendBottom()
+	}
 
 	if (e.deltaY < 0 && e.altKey) {
-		testColumn.appendTop();
-	};
+		testColumn.appendTop()
+	}
 
 	if (e.deltaY > 0 && e.altKey) {
-		testColumn.cutTop();
-	};
+		testColumn.cutTop()
+	}
 
-	render();
-}, false);
+	render()
+}, false)
 
 canvas.element.addEventListener('touchstart', function (e) {
-	e.preventDefault();
+	e.preventDefault()
 
-	state.panning = true;
+	state.panning = true
 
-	state.pointerPosition.h = e.touches[0].clientX;
-	state.pointerPosition.v = e.touches[0].clientY;
+	state.pointerPosition.h = e.touches[0].clientX
+	state.pointerPosition.v = e.touches[0].clientY
 
-	render();
-}, true);
+	render()
+}, true)
 
 canvas.element.addEventListener('touchend', function (e) {
-	e.preventDefault();
+	e.preventDefault()
 
-	state.panning = false;
+	state.panning = false
 
-	render();
-}, false);
+	render()
+}, false)
 
 canvas.element.addEventListener('touchmove', function (e) {
-	e.preventDefault();
+	e.preventDefault()
 
 	state.panDisplacement.h = int(e.touches[0].clientX) -
-		state.pointerPosition.h;
+		state.pointerPosition.h
 	state.panDisplacement.v = int(e.touches[0].clientY) -
-		state.pointerPosition.v;
+		state.pointerPosition.v
 
-	state.pointerPosition.h = int(e.touches[0].clientX);
-	state.pointerPosition.v = int(e.touches[0].clientY);
+	state.pointerPosition.h = int(e.touches[0].clientX)
+	state.pointerPosition.v = int(e.touches[0].clientY)
 
-	render();
-}, false);
+	render()
+}, false)
 
 
 
 function render() {
-	canvas.clear();
+	canvas.clear()
 
-	infoPanel.draw();
+	infoPanel.draw()
 
-	testColumn.draw();
-};
+	testColumn.draw()
+}
 
 function int(x) {
-	return +(x.toFixed());
-};
+	return +(x.toFixed())
+}
